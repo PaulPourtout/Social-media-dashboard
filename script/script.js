37cc5e3e38b20b2e8b24381851f758f76c3d6a29
Paul <paul@kobusapp.com>
2020-04-06 22:37:01 +0200

    fix styling

--- script/script.js
+++ script/script.js
@@ -1,20 +1,20 @@
 const body = document.querySelector("body");
-let toggleState = false;
+let darkThemeState = false;
 const toggleElement = document.querySelector("#toggle");
 const toggleContainerElement = document.querySelector("#toggle-container");
 
 toggleContainerElement.addEventListener("click", toggleTheme);
 
 function toggleTheme() {
-    if (toggleState) {
-        body.classList.remove("dark-theme");
-        body.classList.add("light-theme");
-        toggleElement.classList.remove("toggle--active");
-        toggleState = false;
-    } else {
+    if (!darkThemeState) {
         body.classList.remove("light-theme");
         body.classList.add("dark-theme");
         toggleElement.classList.add("toggle--active");
-        toggleState = true;
+        darkThemeState = true;
+    } else {
+        body.classList.remove("dark-theme");
+        body.classList.add("light-theme");
+        toggleElement.classList.remove("toggle--active");
+        darkThemeState = false;
     }
 }
